145a5ebdd1c42fba6f1018e55b9a95a060386ffa
Priyanshu Singh <singhpriyanshu144@gmail.com>
2026-04-18 15:17:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express()
+
+
+app.get("/check", (req, res)=>{
+    res.send("current version 1.0");
+})
+app.listen(3000, (req, res)=>{
+    console.log("Server listen on port 3000");
+})
