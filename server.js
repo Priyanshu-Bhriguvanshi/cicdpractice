998d254d9a7401aca6a84ca7c5f35b088154aecd
Priyanshu Singh <singhpriyanshu144@gmail.com>
2026-04-19 00:05:40 +0530

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 
 app.get("/check", (req, res)=>{
-    res.send("current version 1.0");
+    res.send("Hello Priyanshu Singh how are you ?  the current version 1.0");
 })
 app.listen(3000, (req, res)=>{
     console.log("Server listen on port 3000");
